9fbbe7652c5c3178e43ffc96f333fb2dd57d293d
yahya zaizi <146640218+Yahyazaizi@users.noreply.github.com>
2023-10-18 14:30:45 +0200

    Add files via upload

---
+++ yahya1.py
@@ -0,0 +1,7 @@
+chaine =input("Ceci est un exemple de chaîne avec des lettres à remplacer.")
+n =input("caractere_a_remplacer")
+r = input("caractere_de_remplacement")
+
+chaine_modifiee = chaine.replace(n, r)
+
+print(chaine_modifiee)
